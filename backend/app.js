da950d860a3f3f97664b11abd3b53d3d68fc2599
Navaneeth dev <navaneethdev33@gmail.com>
2026-06-17 15:52:37 +0530

    fix code

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,10 @@ import cookieParser from "cookie-parser";
 import arcjetMiddleware from "./middleware/arcjet.middleware.js";
 
 
+import cors from "cors";
+
 const app=express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
