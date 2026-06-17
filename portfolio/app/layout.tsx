75689a685eba73e14a0517cd118a73b132047204
Navaneeth dev <navaneethdev33@gmail.com>
2026-06-17 13:58:14 +0530

    add portfolio files

---
+++ portfolio/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import "./globals.css";
+import NavBar from '@/components/NavBar'
+
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Navaneeth Dev G",
+  description: "portfolio development",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${spaceGrotesk.variable}  h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col bg-black selection:bg-cyan-500 selection:text-white">
+
+        <NavBar />
+        {children}</body>
+    </html>
+  );
+}
---
+++ portfolio/lib/data.ts
@@ -0,0 +1,40 @@
+export const projects = [
+
+    {
+        title:"DevEvents",
+        description:"Dynamically create events  and book the events",
+        video:"/videos/dev-events.mp4",
+        tech:["Next.js","Tailwind","MongoDB"],
+        github:"https://github.com/NavaneethDev30/dev-event",
+        live: "https://dev-event-silk.vercel.app/",
+        category: "Personal Project",
+    },
+  {
+    title: "Movie Recommender",
+    description:
+      "AI-powered movie recommendation system using TMDB and machine learning.",
+    video: "/videos/mrs.mp4",
+    tech: [ "Python", "TMDB"],
+    github: "https://github.com/NavaneethDev30/movie_recommendation_system",
+    live: "https://movierecommendationsystem-7fdxgn2jg6njnhqqzgpldb.streamlit.app/",
+    category: "Personal Project",
+  },
+
+  {
+    title: "Multimodel Chatbot",
+    description:
+      "Privacy-first multimodal AI assistant with image recognition, face verification, and syllabus-based conversational support..",
+    video: "/videos/multimodel chatbot.mp4",
+    tech: ["Python", "Laava", "PHI3","Transformers"],
+    github: "https://github.com/NavaneethDev30/multimodal-chatbot",
+    category: "AI Project",
+  },
+  {
+    title:"Personalized Learning path Generator",
+    description:"Built and styled the entire UI using Next.js and Tailwind CSS, including the course creation and interactive quizinterface",
+    video:"/videos/per-lear-path generator.mp4",
+    tech:["Next JS","Taiwind","Figma"],
+    github:"https://github.com/NavaneethDev30/personalized-learning-path-generator",
+    live:"https://personalized-learning-path-generato-virid.vercel.app/",
+  }
+];
