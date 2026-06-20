6176cdff579ee2ab1888c6d90ccefa337a1089ae
Navaneeth dev <navaneethdev33@gmail.com>
2026-06-20 14:20:46 +0530

    remove test.js

--- portfolio/test.js
+++
@@ -1,8 +0,0 @@
-fetch('http://localhost:3000/api/contact', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({name: 'Test', email: 'navaneethdev33@gmail.com', message: 'test next js api'})
-}).then(async res => {
-  console.log('Status:', res.status);
-  console.log('Body:', await res.text());
-}).catch(err => console.error(err));
